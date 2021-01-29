6820e2781bc7c0b17c29954295d30bdb9d0cb61c
romanisthere <romanisthere@gmail.com>
2021-01-29 15:32:38 +0300

    animate through js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const handleClickPrev = (elem, func) => {
 
 
 const gameToggleBtn = $('.gameToggleBtn')
-const gameSection = $('.game')
+const gameSection = $('.game-unique')
 const toggleText = document.querySelectorAll('.glitch_base')
 const handleGameToggleClick = () => {
     const toggleNewText = gameToggleBtn.textContent === 'OFF' ? 'ON' : 'OFF'
@@ -17,3 +17,30 @@ const handleGameToggleClick = () => {
 }
 
 handleClickPrev(gameToggleBtn, handleGameToggleClick)
+
+let gameTimer = null
+const gameCellsUniq = document.querySelectorAll('.game__field-unique .game__cell_wrap')
+gameCellsUniq.forEach(elem => {
+    elem.addEventListener('mouseenter', e => {
+        const dataIndex = e.currentTarget.getAttribute('data-index')
+        const gameCells = document.querySelectorAll(`.game__cell_wrap[data-index="${dataIndex}"]`)
+        gameCells.forEach(item => item.classList.add('game__cell_wrap-hover'))
+
+        gameSection.classList.add('anim-1')
+    })
+
+    elem.addEventListener('mouseleave', e => {
+        const dataIndex = e.currentTarget.getAttribute('data-index')
+        const hoveredCells = document.querySelectorAll('.game__cell_wrap-hover')
+        hoveredCells.forEach(item => item.classList.remove('game__cell_wrap-hover'))
+
+        if (gameTimer) {
+            clearTimeout(gameTimer)
+            gameTimer = null
+        }
+
+        gameTimer = setTimeout(() => {
+            gameSection.classList.remove('anim-1')
+        }, 2500)
+    })
+})
