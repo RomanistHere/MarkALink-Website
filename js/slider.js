61274b2565bd0f3a5bd012560b9c78f0ae98ba9c
romanisthere <romanisthere@gmail.com>
2021-02-10 16:10:24 +0300

    add slider

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 3,
+    grabCursor: true,
+    effect: 'coverflow',
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    initialSlide: 2,
+
+    coverflowEffect: {
+		rotate: 60,
+		stretch: 10,
+		depth: 50,
+		modifier: 1,
+		slideShadows: true,
+	},
+
+    on: {
+        init: () => {
+            document.querySelector('.cases__swiper').classList.add('cases__swiper-show')
+        },
+        click: (swiper, event) => {
+            const target = event.target
+            if (target.classList.contains('swiper-slide-active'))
+                document.querySelector('.cases').classList.add('cases-expanded')
+            else if (target.classList.contains('cases__image'))
+                document.querySelector('.cases').classList.remove('cases-expanded')
+        }
+    },
+})
