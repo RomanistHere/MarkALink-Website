4c54abb4db11bb6d3df14bf95ab68276b053c71f
romanisthere <romanisthere@gmail.com>
2021-03-17 15:11:10 +0300

    cross-browser fixes

--- js/download.js
+++ js/download.js
@@ -42,3 +42,22 @@ downloadLinks.forEach(elem => {
         header.classList.remove('header-download')
     })
 })
+
+const browser = (function() {
+    const test = function(regexp) {return regexp.test(window.navigator.userAgent)}
+    switch (true) {
+        case test(/edg/i): return "Microsoft Edge";
+        case test(/trident/i): return "Microsoft Internet Explorer";
+        case test(/firefox|fxios/i): return "Mozilla Firefox";
+        case test(/opr\//i): return "Opera";
+        case test(/ucbrowser/i): return "UC Browser";
+        case test(/samsungbrowser/i): return "Samsung Browser";
+        case test(/chrome|chromium|crios/i): return "Google Chrome";
+        case test(/safari/i): return "Apple Safari";
+        default: return "Other";
+    }
+})();
+
+if (browser === "Mozilla Firefox") {
+    $('.section').classList.add('weakPerf')
+}
